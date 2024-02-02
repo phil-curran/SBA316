61d0b94342c880e40baea25b14c54c7b4972ac69
Phil Curran <pecurran@outlook.com>
2024-02-02 14:11:04 -0800

    day 2.

--- _js/script.js
+++ _js/script.js
@@ -1,1 +1,73 @@
-console.log("hello world.");
+class Animal {
+  constructor(eyes, legs, isAwake, isMoving) {
+    this.eyes = eyes;
+    this.legs = legs;
+    this.isAwake = isAwake;
+    this.isMoving = isMoving;
+    // return is not needed because the new object is returned by default
+  }
+  sleep() {
+    this.isAwake = false;
+  }
+  wake() {
+    this.isAwake = true;
+  }
+  sit() {
+    this.isMoving = false;
+  }
+  walk() {
+    this.isMoving = true;
+  }
+  speak(sound) {
+    console.log(sound);
+  }
+  toString(animal = "Animal") {
+    return `This ${animal} has ${this.eyes} eyes and ${this.legs} legs. It ${
+      this.isAwake ? "is" : "is not"
+    } awake, and ${this.isMoving ? "is" : "is not"} moving.`;
+  }
+}
+
+class Cat extends Animal {
+  constructor(fur, isAwake, isMoving) {
+    super(2, 2, isAwake, isMoving);
+    this.fur = fur;
+  }
+  speak() {
+    super.speak("Meow...");
+  }
+  toString() {
+    return super.toString("Cat");
+  }
+}
+
+class Human extends Animal {
+  constructor(hair, name, age, location, occupation, isAwake, isMoving) {
+    super(2, 2, isAwake, isMoving);
+    this.hair = hair;
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.occupation = occupation;
+  }
+  greet() {
+    super.speak("Hello!");
+  }
+  toString() {
+    return super.toString("Human");
+  }
+}
+
+const cat1 = new Cat("Orange", true, false);
+const human = new Human(
+  "blond",
+  "Phil",
+  45,
+  "Seattle",
+  "unemployed",
+  true,
+  true
+);
+
+console.log(human);
+console.log(human.speak());
