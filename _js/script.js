a4ff7649a0c8c0cd54d2d180c4d2f75574399b26
Phil Curran <pecurran@outlook.com>
2024-02-03 23:15:40 -0800

    fixed business logic, added some styling.

--- _js/script.js
+++ _js/script.js
@@ -1,42 +1,31 @@
 // instantiate permList that will update localStorage
-var permList = [{}];
 
 // Convert permList array to a JSON string and store it in localStorage
 // sets 'backend' and sets initial state: array with single empty object
-localStorage.setItem("permList", JSON.stringify(permList));
+if (localStorage.getItem("permList") === null) {
+  var permList = [{}];
+  localStorage.setItem("permList", JSON.stringify(permList));
+}
 
 // Retrieve the JSON string from localStorage and parse it back to an array
-// overwrites local tempList with data from localStorage
-const updateTempList = () => {
-  // temp var that pulls from localStorage todo list
-  let temp = JSON.parse(localStorage.getItem("permList"));
+const getList = () => {
   // returns localStorage todo list as array of objects
-  return temp;
+  return JSON.parse(localStorage.getItem("permList"));
 };
 
-let tempList = updateTempList();
-
-console.log("temp list: ", tempList);
-
-const updatePermList = (task) => {
-  // set temp var that = tempList
-  let permList = updateTempList();
-  // push temp object to permList
-  permList.push(temp);
-  // overwrite todo_list with permlist
-  localStorage.setItem("permList", JSON.stringify(permList));
-  // update local version of todo list
-  updateTempList();
-  return permList;
+const addTask = (task) => {
+  // set temp var that = todo list
+  let temp = JSON.parse(localStorage.getItem("permList"));
+  // push temp object to todo list
+  temp.push(task);
+  // overwrite todo_list with new local todo list
+  localStorage.setItem("permList", JSON.stringify(temp));
 };
 
-let permList = updateTempList();
-
-console.log("perm list: ", permList);
-
+// todo model class
 class ToDo {
   constructor(task, urgency, dueDate) {
-    this.id = permList.length;
+    this.id = JSON.parse(localStorage.getItem("permList")).length;
     this.task = task;
     this.dateAdded = new Date();
     this.urgency = urgency;
@@ -45,6 +34,13 @@ class ToDo {
   }
 }
 
+const dateConverter = (date) => {
+  let temp = new Date(date);
+  let month = temp.getMonth() + 1;
+  let day = temp.getDay();
+  let year = temp.getFullYear();
+  return `${month}/${day}/${year}`;
+};
 // // get form:
 let form = document.getElementById("form");
 // destructure form fields:
@@ -52,12 +48,58 @@ const { task, urgency, dueDate } = form;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("task: ", task.value);
-  console.log("urgency: ", urgency.value);
-  console.log("due date: ", dueDate.value);
-  let temp = new ToDo(task.value, urgency.value, dueDate.value);
-  console.log(temp);
-  updatePermList(temp);
-  console.log("updated temp list: ", tempList.length);
-  console.log("updated perm list: ", permList.length);
+  let tempTask = new ToDo(task.value, urgency.value, dueDate.value);
+  addTask(tempTask);
+  drawTable();
 });
+
+// Retrieve the list element
+let list = document.getElementById("list");
+
+const drawTable = () => {
+  let displayList = getList();
+  list.innerHTML = "";
+  console.log("displayList: ", displayList);
+
+  displayList.forEach((item) => {
+    if (item.id > 0) {
+      // Create a new table row
+      let newItem = document.createElement("tr");
+
+      // Create and append table cells
+      let idCell = document.createElement("td");
+      idCell.textContent = item.id;
+      idCell.classList.add("centered-text");
+      newItem.appendChild(idCell);
+
+      let taskCell = document.createElement("td");
+      taskCell.textContent = item.task;
+      newItem.appendChild(taskCell);
+
+      let dateAddedCell = document.createElement("td");
+      dateAddedCell.textContent = dateConverter(item.dateAdded);
+      dateAddedCell.classList.add("centered-text");
+      newItem.appendChild(dateAddedCell);
+
+      let urgencyCell = document.createElement("td");
+      urgencyCell.textContent = item.urgency;
+      newItem.appendChild(urgencyCell);
+
+      let dateDueCell = document.createElement("td");
+      dateDueCell.textContent = dateConverter(item.dueDate);
+      dateDueCell.classList.add("centered-text");
+      newItem.appendChild(dateDueCell);
+
+      let editCell = document.createElement("td");
+      editCell.innerHTML = `<button class="uk-icon-button caution" uk-icon="file-edit"></button>
+      <button class="uk-icon-button warning" uk-icon="trash"></button>`;
+      editCell.classList.add("centered-text", "centered-icons");
+      newItem.appendChild(editCell);
+
+      // Append the new table row to the table
+      list.appendChild(newItem);
+    }
+  });
+};
+
+drawTable();
